0eb5d701d6a3b4d81f8abbba966dcc64924d38b8
sanket karvekar <sksanket7@gmail.com>
2021-01-06 10:32:09 +0530

    Upload files from Spring Initializer

---
+++ src/main/java/com/sanket/SpringBootSecurityUsingJWT/SpringBootSecurityUsingJwtApplication.java
@@ -0,0 +1,13 @@
+package com.sanket.SpringBootSecurityUsingJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityUsingJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityUsingJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanket/SpringBootSecurityUsingJWT/SpringBootSecurityUsingJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket.SpringBootSecurityUsingJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityUsingJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
