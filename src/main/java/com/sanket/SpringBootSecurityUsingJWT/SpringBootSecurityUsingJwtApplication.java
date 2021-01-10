b59dec8f6ed7d129ceced63121dc4c8436349113
sanketkarvekar <sksanket7@gmail.com>
2021-01-11 01:07:00 +0530

    CORS Policy Changes to deal with UI.

--- src/main/java/com/sanket/SpringBootSecurityUsingJWT/SpringBootSecurityUsingJwtApplication.java
+++ src/main/java/com/sanket/SpringBootSecurityUsingJWT/SpringBootSecurityUsingJwtApplication.java
@@ -5,6 +5,10 @@ import com.sanket.SpringBootSecurityUsingJWT.repository.UserRepositoy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -12,6 +16,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
+/*@CrossOrigin(origins = "*")*/
 public class SpringBootSecurityUsingJwtApplication {
 
 	@Autowired
@@ -27,6 +32,17 @@ public class SpringBootSecurityUsingJwtApplication {
 				).collect(Collectors.toList());
 		userRepositoy.saveAll(users);
 	}
+
+	/*@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedHeaders("*").allowedOrigins("*").allowedMethods("*")
+						.allowCredentials(true);
+			}
+		};
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityUsingJwtApplication.class, args);
--- src/main/java/com/sanket/SpringBootSecurityUsingJWT/controller/LoginController.java
+++ src/main/java/com/sanket/SpringBootSecurityUsingJWT/controller/LoginController.java
@@ -6,10 +6,7 @@ import com.sanket.SpringBootSecurityUsingJWT.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
@@ -20,8 +17,10 @@ public class LoginController {
     @Autowired
     private JwtUtil jwtUtil;
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("/")
     public String login(){
+        System.out.println("Calling ********************************************************************");
         return "Welcome to UnderWriter Engine";
     }
 
